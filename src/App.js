7aab8d34ee68e7f2cb827b234e1ce0c1f74dc9f4
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-07-27 23:42:18 +0600

    header section added done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./Components/Page/Header/Header";
 import Navbar from "./Components/Page/Navbar/Navbar";
 
 function App() {
   return (
     <div className="">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Page/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div class="header-section d-flex align-items-end">
+      <div className="container text-white  header-info">
+        <h2 className="">Computer Engineering</h2>
+        <p className="">142,765 Computer Engineers follow this</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
